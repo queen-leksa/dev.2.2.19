a4221c6f24193c44ef5d11432dbe505fb531ea03
Leksa <surnacheva@ithub.ru>
2021-05-13 18:47:30 +0300

    set pug preprocessor

---
+++ 2021.05.13/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const parser = require("body-parser").json();
+const app = express();
+
+app.use(express.urlencoded(true)); // Чтобы получать req.body (методом post)
+app.use(parser);
+app.use(express.static("./public"));
+app.use("/", require("./server/router"));
+
+app.set("view engine", "pug"); // Указываем какой шаблонизатор используем
+app.set("views", "./server/templates"); // Указываем путь к папке с шаблонами
+
+app.listen(5006);
+
+
+/*
+    Препроцессоры
+    .ejs
+    .pug (jade)
+    .hbs (handlebars)
+
+    js => html
+    ul.menu
+        li
+            a Ссылка
+    => <ul class="menu"><li><a>Ссылка</a></li></ul>
+*/
